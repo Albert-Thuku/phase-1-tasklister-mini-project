8c7a383d891ff1acd20fbf190003107285f5d1e3
Albert Thuku <athuku02@gmail.com>
2023-03-27 05:32:16 +0300

    Attempt 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    })
+  }
+  );
+
+  function buildToDo(todo){
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deletebtn)
+    btn.textContent = 'x'
+    li.textContent = `${todo}`
+    li.appendChild(btn)
+    console.log(li)
+    document.querySelector('#tasks').appendChild(li)
+  }
+
+  function deletebtn(e){
+    e.target.parentNode.remove()
+  }
